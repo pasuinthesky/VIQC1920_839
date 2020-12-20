11c164be8539f71a366be8c537f9e1973e61a07e
andesinthesky <andesinthesky@gmail.com>
2020-12-20 11:23:56 -0500

    starting to insert anti gyro drift code

--- RobotC/xBrain_TeleOp_20.c
+++ RobotC/xBrain_TeleOp_20.c
@@ -37,6 +37,9 @@ int iChC_filtered;
 int iChA_filtered;
 int iChB_filtered;
 
+#define GYRO_SAMPLING_SECONDS 1000
+#define ACCEPTABLE_DRIFT_RANGE 0.08
+float fGyroDriftRate;
 /*
 int iDriveMapping[101] = {
 0,0,0,0,0,0,0,0,0,0,
@@ -74,6 +77,41 @@ int iTurnMapping[101] = {
 	30,30,30,30,30,30,30,30,30,30,
 	30,30,30,30,30,30,30,30,30,30,
 	40,40,40,40,40,40,40,60,60,60,60};
+
+void setGyroStable()
+{
+	fGyroDriftRate = 100;
+
+	while (fGyroDriftRate > ACCEPTABLE_DRIFT_RANGE)
+	{
+		// Let the LDH flashing to remind people do not move the robot.
+		setTouchLEDColor(LED,colorRed);
+		setTouchLEDBlinkTime(LED, 14,6);
+		wait1Msec(3);
+		setTouchLEDBlinkTime(LED, 0,1);
+		wait1Msec(2);
+
+		setTouchLEDColor(LED,colorGreen);
+		setTouchLEDBlinkTime(LED, 8, 12);
+
+		resetGyro(Gyro);
+		clearTimer(T4);
+		wait1Msec(GYRO_SAMPLING_SECONDS);
+
+		fGyroDriftRate = getGyroDegreesFloat(Gyro) / GYRO_SAMPLING_SECONDS;
+
+		if (fGyroDriftRate < ACCEPTABLE_DRIFT_RANGE)
+			setTouchLEDColor(LED, colorGreen);
+		else
+			setTouchLEDColor(LED, colorRed);
+
+		setTouchLEDBlinkTime(LED, 4, 4);
+		wait1Msec(1);
+		setTouchLEDColor(LED,colorNone);
+		setTouchLEDBlinkTime(LED, 1, 0);
+	}
+
+}
 
 void eightDirectionalLimitedJoystick()
 {
